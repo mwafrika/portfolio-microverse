ecf175d07b7873870d9511eda94d879c23df84a3
backtofayton <yucelolcay@gmail.com>
2022-01-25 11:24:42 +0200

    Fix linter errors

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const menuContainer = document.querySelector(".mobile-menu-list");
-const menuItem = document.querySelectorAll(".mobile-menu-item");
-const hamburger = document.querySelector(".mobile");
-const closeIcon = document.querySelector(".closeIcon");
-const menuIcon = document.querySelector(".menuIcon");
+const menuContainer = document.querySelector('.mobile-menu-list');
+const menuItem = document.querySelectorAll('.mobile-menu-item');
+const hamburger = document.querySelector('.mobile');
+const closeIcon = document.querySelector('.closeIcon');
+const menuIcon = document.querySelector('.menuIcon');
 
 function toggleMenu() {
-  if (menuContainer.classList.contains("showMenu")) {
-    menuContainer.classList.remove("showMenu");
-    closeIcon.style.display = "none";
-    menuIcon.style.display = "block";
+  if (menuContainer.classList.contains('showMenu')) {
+    menuContainer.classList.remove('showMenu');
+    closeIcon.style.display = 'none';
+    menuIcon.style.display = 'block';
   } else {
-    menuContainer.classList.add("showMenu");
-    closeIcon.style.display = "block";
-    menuIcon.style.display = "none";
+    menuContainer.classList.add('showMenu');
+    closeIcon.style.display = 'block';
+    menuIcon.style.display = 'none';
   }
 }
 
-hamburger.addEventListener("click", toggleMenu);
+hamburger.addEventListener('click', toggleMenu);
 
 menuItem.forEach((item) => {
-  item.addEventListener("click", toggleMenu);
+  item.addEventListener('click', toggleMenu);
 });
